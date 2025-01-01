e91621d06d1f1171d2d1a0927018680e6ec661e6
IT22025180 <it22025180@my.sliit.lk>
2025-01-01 09:36:03 +0530

    Vanuja1

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+//rest
+
+const app = express();
+
+const uri = 'mongodb+srv://vanuja2003:2003vanuja@cluster0.c8ywhvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to mongodb databse');
+    } catch (error) {
+        console.log('MongoDb error: ', error);
+    }
+};
+
+connect();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Welcome to backend',
+    });
+});
+
+app.use(express.json());
+
+//port
+const PORT = process.env.PORT || 4001;
+
+//run
+app.listen(PORT, () => {
+    console.log(`Server is Running on ${PORT}`);
+})
+
