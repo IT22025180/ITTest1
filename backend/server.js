61ca0a70eb6746a6ac05a101b1e231a861aa4c12
IT22025180 <it22025180@my.sliit.lk>
2025-02-07 16:17:59 +0530

    v1

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,8 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is Running on ${PORT}`);
 })
 
+
+
 app.use('/api', routerCus);
 app.use('/api', routerItem);
 app.use('/api', routerRental);
